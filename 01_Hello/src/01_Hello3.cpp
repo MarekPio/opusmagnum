50906919100e4987ba124258ffcccc245d57cacd
MarekPio <marekp18@gmail.com>
2020-01-28 21:40:39 +0100

    Pierwszy program

---
+++ 01_Hello/src/01_Hello3.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// File>New>C/C++ Project>C++ Managed Build (CDT Linux)
+//
+// Name        : 01_Hello3.cpp
+// Author      : Marek Piotrowski
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Witamy na pokładzie" << endl;
+	return 0;
+}
